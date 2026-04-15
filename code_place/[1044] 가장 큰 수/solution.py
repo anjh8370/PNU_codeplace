823567805f51b6f674e2b1646307cfc2d7c17d66
An Jeonghwan <anjh8370@gmail.com>
2026-04-15 16:06:55 +0900

    2026년 04월 15일 16:06:53

--- code_place/[1044] 가장 큰 수/solution.py
+++ code_place/[1044] 가장 큰 수/solution.py
@@ -1,6 +1,3 @@
 number = list(map(str, input().split()))
 number.sort(key = lambda x: x*3, reverse = True)
-if number[0] !='0':
-    print(''.join(number) )
-else:
-    print('0')
+print(''.join(number) if number[0] !='0' else '0' )
